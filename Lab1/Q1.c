76ae3316bb050e440c71aac24bb08e167c1b58bd
Eugene <efoo10@gmail.com>
2022-01-22 15:45:49 +0800

    Update Q1.c

--- Lab1/Q1.c
+++ Lab1/Q1.c
@@ -32,7 +32,7 @@ int main() {
     
     printList(head);
     
-    while(1) {
+    while (1) {
         printf("Enter the index of the node to be removed: ");
         scanf("%d", &index);
         
