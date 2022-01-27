27a6bb7af358e8d9db1cd5915c924dbd1cfc63f7
Eugene <efoo10@gmail.com>
2022-01-27 10:40:19 +0800

    Given Backbone Code

---
+++ Assignment1/Q1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct _listNode{
+    float item;
+    struct _listNode* next;
+};
+typedef struct _listNode ListNode;
+
+void printList(ListNode *head);
+void deleteList(ListNode **ptrHead);
+
+ListNode *reverseSegment(ListNode* head, int start, int end);
+
+int main()
+{
+    ListNode *head = NULL, *temp;
+	float f = 0.0;
+	int S, E;
+
+	scanf("%d %d",&S, &E);
+
+	while (scanf("%f", &f)){
+		if (head == NULL){
+			head = (ListNode*) malloc(sizeof(ListNode));
+			temp = head;
+		}
+		else{
+			temp->next = (ListNode*) malloc(sizeof(ListNode));
+			temp = temp->next;
+		}
+		temp->item = f;
+	}
+	temp->next = NULL;
+
+	head = reverseSegment(head,S,E);
+	printList(head);
+	deleteList(&head);
+	return 0;
+
+}
+
+void printList(ListNode *head){
+    while(head !=NULL){
+        printf("%.2f ",head->item);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+void deleteList(ListNode **ptrHead){
+    ListNode *cur = *ptrHead;
+    ListNode *temp;
+    while (cur!= NULL) {
+		temp=cur->next;
+		free(cur);
+		cur=temp;
+	}
+	*ptrHead=NULL;
+}
+
+ListNode *reverseSegment(ListNode* head, int start, int end)
+{
+// Write your code here
+}
