e960b50c1401bd364fcddc63601277b182089156
Eugene <efoo10@gmail.com>
2022-02-01 21:08:23 +0800

    Update Q2.c

--- Assignment1/Q2.c
+++ Assignment1/Q2.c
@@ -22,9 +22,11 @@ typedef struct _linkedList {
     int size;
 } LinkedList;
 
+void printListX(ListNode *head);
 void printList(LinkedList llist);
 void deleteList(LinkedList *llptr);
-LinkedList rearrange (LinkedList llist);
+
+void rearrange (LinkedList llist);
 
 int main() {
     LinkedList llist;
@@ -50,15 +52,20 @@ int main() {
         llist.size++;
     }
     temp->next = NULL;
+
+    //llist = rearrange(llist);
+    //printList(llist);
     
-    printf("\nAFTER USER INPUT: \n"); // FOR DEBUG
-    printList(llist); // FOR DEBUG
-    
-    llist = rearrange(llist);
-    
-    printf("\nAFTER REARRANGE: \n"); // FOR DEBUG
-    printList(llist);
-    deleteList(&llist);
+    rearrange(llist);
+    //deleteList(&llist);
+}
+
+void printListX(ListNode *head) {
+    while (head != NULL) {
+        printf("%d ", head->item);
+        head = head->next;
+    }
+    printf("\n");
 }
 
 void printList(LinkedList llist) {
@@ -85,6 +92,191 @@ void deleteList(LinkedList* llptr) {
     llptr->size = 0;
 }
 
-LinkedList rearrange(LinkedList llist) {
-    return llist;
+void rearrange(LinkedList llist) {
+    int listSize = llist.size;
+    //printf("ListSize: %d\n", listSize);
+    
+    int halfSize = listSize/2;
+    
+    int splitLeftCounter = 0;
+    int splitRightCounter = 0;
+    int finalCounter = 0;
+    
+    ListNode *splitLeftOne = NULL;
+    ListNode *splitRightOne = NULL;
+    ListNode *splitOne = NULL;
+    
+    ListNode *splitLeftTwo = NULL;
+    ListNode *splitRightTwo = NULL;
+    ListNode *splitTwo = NULL;
+    
+    ListNode *temp = llist.head;
+    ListNode *aTemp;
+    
+    ListNode *tempLeft = NULL;
+    ListNode *tempRight = NULL;
+    
+    while (splitLeftCounter < halfSize && temp != NULL) {
+        //insertNode(&splitLeftOne, splitLeftCounter, temp->item);
+        if (splitLeftOne == NULL) {
+            splitLeftOne = (ListNode*) malloc(sizeof(ListNode));
+            aTemp = splitLeftOne;
+        }
+        else {
+            aTemp->next = (ListNode*) malloc(sizeof(ListNode));
+            aTemp = aTemp->next;
+        }
+        aTemp->item = temp->item;
+        
+        splitLeftCounter++;
+        temp = temp->next;
+    }
+    
+    while (splitRightCounter < (listSize - halfSize) && temp != NULL) {
+        //insertNode(&splitRightOne, splitRightCounter, temp->item);
+        if (splitRightOne == NULL) {
+            splitRightOne = (ListNode*) malloc(sizeof(ListNode));
+            aTemp = splitRightOne;
+        }
+        else {
+            aTemp->next = (ListNode*) malloc(sizeof(ListNode));
+            aTemp = aTemp->next;
+        }
+        aTemp->item = temp->item;
+        
+        splitRightCounter++;
+        temp = temp->next;
+    }
+    
+    //printListX(splitLeftOne);
+    //printListX(splitRightOne);
+    
+    tempLeft = splitLeftOne;
+    tempRight = splitRightOne;
+    
+    while (finalCounter < listSize) {
+        //insertNode(&splitOne, finalCounter, tempRight->item);
+        if (splitOne == NULL) {
+            splitOne = (ListNode*) malloc(sizeof(ListNode));
+            aTemp = splitOne;
+        }
+        else {
+            aTemp->next = (ListNode*) malloc(sizeof(ListNode));
+            aTemp = aTemp->next;
+        }
+        aTemp->item = tempRight->item;
+        
+        
+        tempRight = tempRight->next;
+        finalCounter++;
+        
+        if (tempLeft != NULL) {
+            //insertNode(&splitOne, finalCounter, tempLeft->item);
+            if (splitOne == NULL) {
+                splitOne = (ListNode*) malloc(sizeof(ListNode));
+                aTemp = splitOne;
+            }
+            else {
+                aTemp->next = (ListNode*) malloc(sizeof(ListNode));
+                aTemp = aTemp->next;
+            }
+            aTemp->item = tempLeft->item;
+            
+            
+            
+            tempLeft = tempLeft->next;
+            finalCounter++;
+        }
+    }
+    
+    //printf("SPLIT ONE\n");
+    //printListX(splitOne);
+    //printf("\n");
+    
+    
+    // SECOND SPLIT
+    
+    splitLeftCounter = 0;
+    splitRightCounter = 0;
+    finalCounter = 0;
+    
+    temp = splitOne;
+    
+    while (splitLeftCounter < halfSize && temp != NULL) {
+        //insertNode(&splitLeftTwo, splitLeftCounter, temp->item);
+        if (splitLeftTwo == NULL) {
+            splitLeftTwo = (ListNode*) malloc(sizeof(ListNode));
+            aTemp = splitLeftTwo;
+        }
+        else {
+            aTemp->next = (ListNode*) malloc(sizeof(ListNode));
+            aTemp = aTemp->next;
+        }
+        aTemp->item = temp->item;
+        
+        splitLeftCounter++;
+        temp = temp->next;
+    }
+    
+    while (splitRightCounter < (listSize - halfSize) && temp != NULL) {
+        //insertNode(&splitRightTwo, splitRightCounter, temp->item);
+        if (splitRightTwo == NULL) {
+            splitRightTwo = (ListNode*) malloc(sizeof(ListNode));
+            aTemp = splitRightTwo;
+        }
+        else {
+            aTemp->next = (ListNode*) malloc(sizeof(ListNode));
+            aTemp = aTemp->next;
+        }
+        aTemp->item = temp->item;
+        
+        splitRightCounter++;
+        temp = temp->next;
+    }
+    
+    //printListX(splitLeftTwo);
+    //printListX(splitRightTwo);
+    
+    tempLeft = splitLeftTwo;
+    tempRight = splitRightTwo;
+    
+    while (finalCounter < listSize) {
+        if (tempLeft != NULL) {
+            //insertNode(&splitTwo, finalCounter, tempLeft->item);
+            if (splitTwo == NULL) {
+                splitTwo = (ListNode*) malloc(sizeof(ListNode));
+                aTemp = splitTwo;
+            }
+            else {
+                aTemp->next = (ListNode*) malloc(sizeof(ListNode));
+                aTemp = aTemp->next;
+            }
+            aTemp->item = tempLeft->item;
+            
+            
+            
+            
+            tempLeft = tempLeft->next;
+            finalCounter++;
+        }
+        
+        //insertNode(&splitTwo, finalCounter, tempRight->item);
+        if (splitTwo == NULL) {
+            splitTwo = (ListNode*) malloc(sizeof(ListNode));
+            aTemp = splitTwo;
+        }
+        else {
+            aTemp->next = (ListNode*) malloc(sizeof(ListNode));
+            aTemp = aTemp->next;
+        }
+        aTemp->item = tempRight->item;
+        
+        tempRight = tempRight->next;
+        finalCounter++;
+    }
+    
+    printListX(splitTwo);
+    
+    deleteList(&llist);
+    //return llist;
 }
